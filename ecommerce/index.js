24e0a7be3a7c5e18b954c00806d0838531ef013f
shashidharreddydakuri <d.shashidhar1996@gmail.com>
2022-10-16 23:37:46 +0530

    post method added

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -13,6 +13,8 @@ mongoose
 		console.log(err);
 	});
 
+	app.use(express.json())
+
 app.use('/api/user', userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
--- ecommerce/routes/user.js
+++ ecommerce/routes/user.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 
 router.get('/usertest', (req, res) => {
-	RTCRtpSender.res('user test success');
+	res.send('user test success');
+});
+
+router.post('/userposttest', (req, res) => {
+	const username = req.body.username;
+	res.send('your username is ' + username);
 });
 
 module.exports = router;
