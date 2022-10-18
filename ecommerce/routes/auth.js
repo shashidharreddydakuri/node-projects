fb02c8d1ccd6881f7716fefd7bae5f891afadbbb
shashidharreddydakuri <d.shashidhar1996@gmail.com>
2022-10-18 23:27:05 +0530

    Register route is created

--- ecommerce/routes/auth.js
+++ ecommerce/routes/auth.js
@@ -1,3 +1,14 @@
 const router = require('express').Router();
+const { request } = require('express');
+const User = require('../models/User');
+
+//Register
+router.post('/register', (re, res) => {
+	const newUser = new User({
+		username: req.body.username,
+		email: req.body.email,
+        password: req.body.password,
+	});
+});
 
 module.exports = router;
