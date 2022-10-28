9af89bba381d7750fd8d78f28a5d7143d946bbc1
shashidharreddydakuri <d.shashidhar1996@gmail.com>
2022-10-28 23:43:53 +0530

    working on jwt CRUD Operations

--- ecommerce/routes/verifyToken.js
+++ ecommerce/routes/verifyToken.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
 	const authHeader = req.headers.token;
 	if (authHeader) {
+		const token = authHeader.split(' ')[1];
 		jwt.verify(token, process.env.JWT_SEC, (err, user) => {
 			if (err) res.status(403).json('Token is not valid');
 			req.user = user;
@@ -18,7 +19,7 @@ verifyTokenAndAuthorization = (req, res, next) => {
 		if (req.user.id === req.params.id || req.user.isAdmin) {
 			next();
 		} else {
-			res.status(403).json('tou are not allowed to do that');
+			res.status(403).json('you are not allowed to do that');
 		}
 	});
 };
