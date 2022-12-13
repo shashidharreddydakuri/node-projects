33f39bffa4f8426e5e424abc438014cc7fc506ab
shashidharreddydakuri <d.shashidhar1996@gmail.com>
2022-12-13 11:16:53 +0530

    Topbar and dark/light mode comleted

--- react-projects/react-admin/src/App.js
+++ react-projects/react-admin/src/App.js
@@ -1,6 +1,20 @@
-import { ColorModeContext, colorModeContext, useMode } from "./theme";
-import { CssBaseline, ThemeProvider} from "@mui/material";
-import Topbar from "./scenes/global/Topbar";
+import React from 'react';
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+import Topbar from './scenes/global/Topbar';
+import Sidebar from './scenes/global/Sidebar';
+import Dashboard from './scenes/dashboard';
+// import Team from "./scenes/team";
+// import Invoices from './scenes/invoices';
+// import Contacts from './scenes/contacts';
+// import Bar from './scenes/bar';
+// import Form from './scenes/form';
+// import Line from './scenes/line';
+// import Pie from './scenes/pie';
+// import Faq from './scenes/faq';
+// import Geography from './scenes/geography';
+// import Calender from './scenes/calender';
 
 function App() {
 	const [theme, colorMode] = useMode();
@@ -10,10 +24,24 @@ function App() {
 			<ThemeProvider theme={theme}>
 				<CssBaseline />
 				<div className="app">
+					<Sidebar />
 					<main className="content">
 						<Topbar />
+						<Routes>
+							<Route path="/" elements={<Dashboard />} />
+							{/* <Route path="/team" elements={<Team />} /> */}
+							{/* <Route path="/contacts" elements={<Contacts />} /> */}
+							{/* <Route path="/invoices" elements={<Invoices />} /> */}
+							{/* <Route path="/form" elements={<Form />} /> */}
+							{/* <Route path="/bar" elements={<Bar />} /> */}
+							{/* <Route path="/pie" elements={<Pie />} /> */}
+							{/* <Route path="/line" elements={<Line />} /> */}
+							{/* <Route path="/faq" elements={<Faq />} /> */}
+							{/* <Route path="/geography" elements={<Geography />} /> */}
+							{/* <Route path="/calender" elements={<Calender />} /> */}
+						</Routes>
 					</main>
-					</div>
+				</div>
 			</ThemeProvider>
 		</ColorModeContext.Provider>
 	);
--- react-projects/react-admin/src/scenes/global/Sidebar.jsx
+++ react-projects/react-admin/src/scenes/global/Sidebar.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const Sidebar = () => {
     return <div>Sidebar</div>
 }
