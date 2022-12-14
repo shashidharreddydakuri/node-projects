38ab945806389d58c86a872f2cf134e243b75f39
shashidharreddydakuri <d.shashidhar1996@gmail.com>
2022-12-14 23:19:46 +0530

    sidebar completed

--- react-projects/react-admin/src/scenes/global/Sidebar.jsx
+++ react-projects/react-admin/src/scenes/global/Sidebar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useState } from 'react';
 import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
-import 'react-pro-sidebar/dist/css/styles.css';
 import { Box, IconButton, Typography, useTheme } from '@mui/material';
 import { Link } from 'react-router-dom';
+import 'react-pro-sidebar/dist/css/styles.css';
 import { tokens } from '../../theme';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
@@ -11,10 +11,10 @@ import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
 import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
-import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined';
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
 import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
-import PieChartOutlinedIcon from '@mui/icons-material/PieChartOutlined';
-import TimeLineOutlinedIcon from '@mui/icons-material/TimeLineOutlined';
+import PieChartOutlineOutlinedIcon from '@mui/icons-material/PieChartOutlineOutlined';
+import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
 
@@ -81,7 +81,7 @@ const Sidebar = () => {
 								ml="15px"
 							>
 								<Typography variant="h3" color={colors.grey[100]}>
-									ADMINIS
+									ADMINS
 								</Typography>
 								<IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
 									<MenuOutlinedIcon />
@@ -108,7 +108,7 @@ const Sidebar = () => {
 									fontWeight="bold"
 									sx={{ m: '10px 0 0 0' }}
 								>
-									Ed Roh
+									Shashi
 								</Typography>
 								<Typography variant="h5" color={colors.greenAccent[500]}>
 									VP Fancy Admin
