b274f8073b20f12da17261fa09ff7c6906c6da8b
shashidharreddydakuri <d.shashidhar1996@gmail.com>
2023-01-07 21:41:39 +0530

    working project

--- geektrust/course-scheduling/geektrust.js
+++ geektrust/course-scheduling/geektrust.js
@@ -47,9 +47,4 @@ data = fs.readFileSync(process.argv[2]).toString();
 main(data);
 
 module.exports = { main };
-// fs.readFile(filename, "utf8", (err, data) => {
-//     /*if (err) throw err
-//     var inputLines = data.toString().split("\n")
-//     // Add your code here to process input commands
-//     */
-// })
+
